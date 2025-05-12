be9def1055e774f3deaf13627be1aa017e7d3452
Quriosity <131264405+Quriosity0@users.noreply.github.com>
2025-05-12 20:00:15 +0000

    returned testing parallel downloads

--- stage3.sh
+++ stage3.sh
@@ -135,6 +135,9 @@ else
     systemctl enable NetworkManager
     clear
 
+    # changing parallel downloads for pacman
+    # sed -i 's/^#\?ParallelDownloads = .*/ParallelDownloads = 15/' /etc/pacman.conf
+
     # Настройка sudo
     echo "Configuring sudo for wheel group..."
     sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
