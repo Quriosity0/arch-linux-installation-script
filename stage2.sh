804e3be463f8285602542fa6789ca6685f55ebe0
Quriosity <131264405+Quriosity0@users.noreply.github.com>
2025-05-12 12:50:21 +0000

    removed old checks and cp's

--- stage2.sh
+++ stage2.sh
@@ -15,29 +15,11 @@ echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
 echo "ru_RU.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
 
-# Жёсткая проверка всех файлов
-required_files=("locale.conf" "vconsole.conf" "hostname")
-missing_files=()
-
-for file in "${required_files[@]}"; do
-    [ ! -f "/tmp/InstallScript/RequiredFiles/$file" ] && missing_files+=("$file")
-done
-
-if [ ${#missing_files[@]} -gt 0 ]; then
-    echo "Error: Missing required files: ${missing_files[*]}"
-    echo "Redownload script and copy it in /tmp"
-    exit 1
-fi
-
-
-# cp /tmp/InstallScript/RequiredFiles/locale.conf /etc/
 cd /etc/
 echo LANG=en_US.UTF-8 > locale.conf
 
-# cp /tmp/InstallScript/RequiredFiles/vconsole.conf /etc/
 echo KEYMAP=us > vconsole.conf
 
-# cp /tmp/InstallScript/RequiredFiles/hostname /etc/
 echo asus-pc > hostname
 
 mkinitcpio -P
